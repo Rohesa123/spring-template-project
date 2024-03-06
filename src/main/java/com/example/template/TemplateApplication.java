c751ba446e2084177c68be3a6b09787d40ce2fff
Rohesa123 <rohesasidiqpermana05@gmail.com>
2024-03-06 16:54:47 +0700

    [UPDATE]: add new packages

--- src/main/java/com/example/template/TemplateApplication.java
+++ src/main/java/com/example/template/TemplateApplication.java
@@ -1,13 +1,25 @@
 package com.example.template;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.SpringVersion;
 
 @SpringBootApplication
-public class TemplateApplication {
+public class TemplateApplication implements ApplicationRunner {
+
+	private static final Logger logger = LogManager.getLogger(TemplateApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(TemplateApplication.class, args);
 	}
 
+	@Override
+	public void run(ApplicationArguments applicationArguments) throws Exception {
+		logger.info("Version: " + SpringVersion.getVersion());
+		logger.info("### Application Started ###");
+	}
 }
